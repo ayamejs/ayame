5dd2cdbcc3808a88a01a050b540fb2e019f39889
pollen5 <31079629+pollen5@users.noreply.github.com>
2020-08-05 15:53:19 +0430

    add eval and auto fetch owner if not given

--- src/core/events/ready.js
+++ src/core/events/ready.js
@@ -2,6 +2,11 @@ const { Event } = require("ayame");
 
 class ReadyEvent extends Event {
   async run() {
+    if(!this.client.options.owner) {
+      const { owner } = await this.client.fetchApplication();
+      this.client.options.owner = owner.id;
+    }
+
     // Initialize all stores.
     await Promise.all(this.client.stores.map((store) => store.init()));
 
