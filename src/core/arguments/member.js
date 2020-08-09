7a21fef9d7e0b3de2455232ccc377c3635994792
pollen5 <31079629+pollen5@users.noreply.github.com>
2020-08-09 19:52:15 +0430

    some arguments i guess

--- src/core/arguments/member.js
+++ src/core/arguments/member.js
@@ -1,6 +1,6 @@
 const { Argument } = require("ayame");
 
-class User extends Argument {
+class Member extends Argument {
   constructor(...args) {
     super(...args);
     this.regex = /^(?:<@!?)?(\d{17,19})>?$/;
@@ -8,14 +8,14 @@ class User extends Argument {
 
   async run(msg, arg, tag) {
     if(this.regex.test(arg)) {
-      const user = this.client.users.fetch(this.regex.exec(arg)[1])
+      const member = msg.guild.members.fetch(this.regex.exec(arg)[1])
         .catch(() => null);
 
-      if(user) return user;
+      if(member) return member;
     }
 
     throw `**${tag.name}** must be a valid user mention or ID.`;
   }
 }
 
-module.exports = User;
+module.exports = Member;
