fbbd5c852baaeed09f5085b938deaa705293e335
Jordan Jones <jordpjones@gmail.com>
2020-08-12 12:28:14 -0700

    Remove isOwner -> owners.has() set

--- src/core/inhibitors/disabled.js
+++ src/core/inhibitors/disabled.js
@@ -2,7 +2,7 @@ const { Inhibitor } = require("ayame");
 
 class DisabledInhibitor extends Inhibitor {
   async run(msg, command) {
-    if(!command.enabled && !this.client.isOwner(msg.author))
+    if(!command.enabled && !this.client.owners.has(msg.author))
       return "That command has been disabled by a bot owner.";
   }
 }
