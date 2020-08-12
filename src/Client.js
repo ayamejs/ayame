c92e8e45efbdbc573962926c183bd206a890ab7a
Jordan Jones <jordpjones@gmail.com>
2020-08-11 21:00:41 -0700

    JSDOC + PLUGIN

--- src/Client.js
+++ src/Client.js
@@ -19,19 +19,52 @@ const { DefaultOptions } = require("./utils/constants.js");
 const plugins = new Set();
 
 class AyameClient extends Client {
+  /**
+ * Constructs the client.
+ * @param {Options} [options = {}] Options to pass to the default client.
+ */
   constructor(options = {}) {
     super(mergeDefault(DefaultOptions, options));
 
-    this.console = options.console || new AyameConsole();
+    /**
+     * The AyameConsole for this client.
+     * @type {AyameConsole}
+     */
+    this.console = options.console || new AyameConsole(this.options.console);
 
+    /**
+     * The directory where all users files are stored at.
+     */
     this.userBaseDirectory = dirname(require.main.filename);
 
-    // Stores.
+    /**
+     * This is where all commands are stored.
+     * @type {CommandStore}
+     */
     this.commands = new CommandStore(this);
+
+    /**
+     * This is where all events are stored.
+     * @type {EventStore}
+     */
     this.events = new EventStore(this);
+
+    /**
+     * This is where all monitors are stored.
+     * @type {MonitorStore}
+     */
     this.monitors = new MonitorStore(this);
+
+    /**
+     * This is where all inhibitors are stored.
+     * @type {InhibitorStore}
+     */
     this.inhibitors = new InhibitorStore(this);
     
+    /**
+     * The registry for all stores.
+     * @type {external:Collection}
+     */
     this.stores = new Collection();
     
     this.registerStore(this.commands)
@@ -41,6 +74,12 @@ class AyameClient extends Client {
 
     for(const store of this.stores.values()) store.registerDirectory(join(__dirname, "core"));
   }
+
+  /**
+   * Use function or string to resolve the current prefix.
+   * @param {Message} msg The message to check against.
+   * @returns {String}
+   */
 
   async getPrefix(msg) {
     if(typeof this.options.prefix === "function") return this.options.prefix(msg);
@@ -71,11 +110,30 @@ class AyameClient extends Client {
     return super.login(token);
   }
 
-  async init() {
-    const loaded = await Promise.all(this.stores.map((store) => store.loadAll().then((size) => [size, store])));
-
-    for(const store of loaded) this.emit("piecesLoaded", store[1].name, store[0]);
+  /**
+   * Uses a plugin module in AyameClient.
+   * @param {Object} plugin The module of the plugin we're using.
+   * @returns {this}
+   * @chainable
+   */
+  static use(plugin) {
+    const plugin = mod[this.plugin];
+    if (typeof plugin !== 'function') throw new TypeError('The provided module does not include a plugin function.');
+    plugins.add(plugin);
+    return this;
   }
 }
 
 module.exports = AyameClient;
+
+/**
+ * The base permissions that are required by discord (somewhere). [VIEW_CHANNEL, SEND_MESSAGES]
+ * @type {Permissions}
+ */
+AyameClient.basePermissions = new Permissions(3072);
+
+/**
+ * The plugin symbol to be used in external packages.
+ * @type {Symbol}
+ */
+AyameClient.plugin = Symbol('AyamePlugin');
